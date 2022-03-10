8dc326d1f67a56aa01045f2b4039cce104861a18
Andrew Nourry <andrewnourry@gmail.com>
2022-03-10 03:32:20 +0100

    corrections sur le fichier Scrape_produit.py

--- Scrape_produit.py
+++ Scrape_produit.py
@@ -34,10 +34,10 @@ def scrape(url, en_tete):
 
     elif en_tete == 'desc':
         des = soup.findAll('p', class_='')
-        desc = []
+        soupdes = []
         for d in des:
-            desc.append(d.string)
-        return desc
+            soupdes.append(d.string)
+        return soupdes
 
     elif en_tete == 'cat':
         a = soup.findAll('a')
@@ -53,39 +53,41 @@ def scrape(url, en_tete):
 
     elif en_tete == 'img':
         img = soup.find('img')
-        lien = img['src']
-        img_url = ['http://books.toscrape.com/' + lien[6:]]
-        return img_url
+        i_lien = img['src']
+        i_url = 'http://books.toscrape.com/' + i_lien[6:]
+        return i_url
 
     else:
         print('Argument invalide')
 
 
-lien = 'http://books.toscrape.com/catalogue/dune-dune-1_151/index.html'
-
-page_url = [scrape(lien, 'link')]
-upc = [scrape(lien, 'upc')]
-titre = [scrape(lien, 'title')]
-price_inclu_tax = [scrape(lien, 'ttc')]
-price_exclu_tax = [scrape(lien, 'ht')]
-quantite = [scrape(lien, 'quantity')]
-description = scrape(lien, 'desc')
-category = [scrape(lien, 'cat')]
-review = [scrape(lien, 'rev')]
-img_url = scrape(lien, 'img')
-
-# liste des informations
-en_tetes = ['product_page_url', 'universal_ product_code', 'title', 'price_including_tax',
-           'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-
-# CSV
-with open('infos_produit.csv', 'w') as file:
-    writer = csv.writer(file, delimiter=',')
-    writer.writerow(en_tete)
-
-    for infos in zip(page_url, upc, titre, price_inclu_tax, price_exclu_tax, quantite, description, category,
-                     review, img_url):
-        writer.writerow(infos)
-
 if __name__ == '__main__':
+
+    lien = 'http://books.toscrape.com/catalogue/dune-dune-1_151/index.html'
+
+    page_url = [scrape(lien, 'link')]
+    upc = [scrape(lien, 'upc')]
+    titre = [scrape(lien, 'title')]
+    price_inclu_tax = [scrape(lien, 'ttc')]
+    price_exclu_tax = [scrape(lien, 'ht')]
+    quantite = [scrape(lien, 'quantity')]
+    description = scrape(lien, 'desc')
+    category = [scrape(lien, 'cat')]
+    review = [scrape(lien, 'rev')]
+    img_url = [scrape(lien, 'img')]
+
+    # liste des informations
+    en_tetes = ['product_page_url', 'universal_ product_code', 'title', 'price_including_tax',
+                'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating',
+                'image_url']
+
+    # CSV
+    with open('infos_produit.csv', 'w') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(en_tetes)
+
+        for infos in zip(page_url, upc, titre, price_inclu_tax, price_exclu_tax, quantite, description, category,
+                         review, img_url):
+            writer.writerow(infos)
+
     print("fichier infos_produit.csv créé avec succès !")

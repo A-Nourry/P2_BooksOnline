1a61cdb7ebd4344f2716f6fb06403c270bc0d66f
Andrew Nourry <andrewnourry@gmail.com>
2022-03-11 19:23:13 +0100

    mise à jour du fichier Scrape_all.py
    script fonctionnel mais lent

--- Scrape_all.py
+++ Scrape_all.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import Scrape_produit
+import Scrape_categorie
 from collections import defaultdict
 
 
@@ -17,11 +18,12 @@ def get_key(val, my_dict):
 # Récupération des liens
 liens = []
 
-for i in range(1, 2):
+for i in range(1, 51):
     url = 'http://books.toscrape.com/catalogue/page-' + str(i) + '.html'
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.content, 'html.parser')
     base = 'http://books.toscrape.com/catalogue/'
+    print('lien de la page ' + str(i) + ' récupéré (' + url + ')')
 
     links = soup.findAll('h3')
 
@@ -30,70 +32,54 @@ for i in range(1, 2):
         lin = a['href']
         liens.append(base + lin)
 
-# Récupération des catégories
-categories = []
-
-for books in liens:
-    cat = Scrape_produit.scrape(books, 'cat')
-    categories.append(cat) if cat not in categories else categories
-
 # Tri des liens par catégories
 links_by_categories = {}
 
 for book in liens:
     links_by_categories[book] = Scrape_produit.scrape(book, 'cat')
+    print('catégorie du livre ' + book + ' récupérée')
 
 links_by_categories_inv = defaultdict(list)
 [links_by_categories_inv[v].append(k) for k, v in links_by_categories.items()]
 dict_cat = dict(links_by_categories_inv)
+'''lien = dict_cat[categories[i]]'''
 
-lien0 = ', ' .join(dict_cat[categories[0]])
+# Récupération des catégories dans une liste
+categories = []
 
+for books in links_by_categories.keys():
+    cat = Scrape_produit.scrape(books, 'cat')
+    categories.append(cat) if cat not in categories else categories
+    print('catégorie ' + links_by_categories[str(books)] + ' récupérée')
+    if len(categories) >= 50:
+        break
+print(categories)
 
-'''# Récupérations des infos pour chaque lien
-page_url = []
-upc = []
-titre = []
-price_inclu_tax = []
-price_exclu_tax = []
-quantite = []
-description = []
-category = []
-review = []
-img_url = []
+all_infos = {}
 
-for books in liens:
-    s_url = Scrape_produit.scrape(books, 'link')
-    page_url.append(s_url)
+for i in range(50):
+    for keys in links_by_categories_inv[categories[i]]:
+        all_infos["cat{0}".format(i)] = dict_cat[categories[i]]
 
-    s_upc = Scrape_produit.scrape(books, 'upc')
-    upc.append(s_upc)
+infos_scrape = {}
 
-    s_titre = Scrape_produit.scrape(books, 'title')
-    titre.append(s_titre)
+for i in range(50):
+    for links in all_infos['cat' + str(i)]:
+        test = [Scrape_categorie.get_info(links)]
+        infos_scrape["lien{0}".format(i)] = test
+        print('page' + links + ' scrapé avec succès')
 
-    pit = Scrape_produit.scrape(books, 'ttc')
-    price_inclu_tax.append(pit[1:])
+# CSV
+en_tetes = ['product_page_url', 'universal_ product_code', 'title', 'price_including_tax',
+            'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating',
+            'image_url']
 
-    pet = Scrape_produit.scrape(books, 'ht')
-    price_exclu_tax.append(pet[1:])
+for info in range(50):
+    with open('Output/Categories/' + categories[info] + '.csv', 'w', encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(en_tetes)
 
-    qts = Scrape_produit.scrape(books, 'quantity')
-    quantite.append(qts)
+        for infos in infos_scrape["lien" + str(info)]:
+            writer.writerow(infos)
 
-    s_des = Scrape_produit.scrape(books, 'desc')
-    description.append(s_des)
-
-    revs = Scrape_produit.scrape(books, 'rev')
-    review.append(revs)
-
-    imgs = Scrape_produit.scrape(books, 'img')
-    img_url.append(imgs)
-
-    cate = Scrape_produit.scrape(books, 'cat')
-
-    category.append(cate)
-
-Scrape_produit.mkcsv('infos_cat.csv', page_url, upc, titre, price_inclu_tax, price_exclu_tax, quantite, description,
-                     category,
-                     review, img_url)'''
+        print("le fichier infos_" + categories[info] + ".csv a été généré avec succès !")

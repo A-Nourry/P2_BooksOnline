101ceba96e15c9412d9794326d9b1175c3a567ec
Andrew Nourry <andrewnourry@gmail.com>
2022-03-15 22:49:08 +0100

    modification du résultat de l'argument "rev" de la fonction scrape()

--- Scrape_produit.py
+++ Scrape_produit.py
@@ -48,8 +48,30 @@ def scrape(url, en_tete):
         return categorie
 
     elif en_tete == 'rev':
-        result = tds[6]
-        return result.text
+        nb_stars = []
+        result = []
+        revs = soup.findAll('div', {'class': "col-sm-6 product_main"})
+
+        for reviews in revs:
+            star = reviews.find('p', class_=lambda value: value and value.startswith("star-rating"))
+            stars = star['class']
+            nb_stars.append(stars)
+
+        if 'One' in nb_stars[0]:
+            result.append('1 sur 5')
+            return ', '.join(result)
+        elif 'Two' in nb_stars[0]:
+            result.append('2 sur 5')
+            return ', '.join(result)
+        elif 'Three' in nb_stars[0]:
+            result.append('3 sur 5')
+            return ', '.join(result)
+        elif 'Four' in nb_stars[0]:
+            result.append('4 sur 5')
+            return ', '.join(result)
+        elif 'Five' in nb_stars[0]:
+            result.append('5 sur 5')
+            return ', '.join(result)
 
     elif en_tete == 'img':
         img = soup.find('img')
@@ -59,21 +81,6 @@ def scrape(url, en_tete):
 
     else:
         print('Argument invalide')
-
-
-def mkcsv(file_name, en1, en2, en3, en4, en5, en6, en7, en8, en9, en10):
-    en_tetes = ['product_page_url', 'universal_ product_code', 'title', 'price_including_tax',
-                'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating',
-                'image_url']
-
-    with open('Output/' + file_name, 'w', encoding="utf-8") as file:
-        writer = csv.writer(file, delimiter=',')
-        writer.writerow(en_tetes)
-
-        for infos in zip(en1, en2, en3, en4, en5, en6, en7, en8, en9, en10):
-            writer.writerow(infos)
-
-    print("le fichier " + file_name + " a été créé avec succès !")
 
 
 if __name__ == '__main__':
@@ -90,5 +97,18 @@ if __name__ == '__main__':
     review = [scrape(lien, 'rev')]
     img_url = [scrape(lien, 'img')]
 
-    mkcsv('infos_produit.csv', page_url, upc, titre, price_inclu_tax, price_exclu_tax, quantite, description, category,
-          review, img_url)
+    # CSV
+
+    en_tetes = ['product_page_url', 'universal_ product_code', 'title', 'price_including_tax',
+                'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating',
+                'image_url']
+
+    with open('infos_produit_Dune.csv', 'w', encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(en_tetes)
+
+        for infos in zip(page_url, upc, titre, price_inclu_tax, price_exclu_tax, quantite, description, category,
+                         review, img_url):
+            writer.writerow(infos)
+
+    print("le fichier infos_produit_Dune.csv a été généré avec succès !")
